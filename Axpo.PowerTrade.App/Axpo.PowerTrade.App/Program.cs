755efe5e0b1b207af371d9f3e95eedcef0cf7357
keskinsinancan <keskinsinancan@users.noreply.github.com>
2024-10-12 18:02:33 +0200

    initial commit

---
+++ Axpo.PowerTrade.App/Axpo.PowerTrade.App/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Program program = new Program();
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+
+
+}
+
