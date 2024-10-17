70e47d7349932d39060ab248515a3400cd2a1b78
Sinan Can Keskin <skeskin@riamoneytransfer.com>
2024-10-17 10:12:55 +0200

    cleanup

--- Axpo.PowerTrade.App/Axpo.PowerTrade.Application/Constants/Constants.cs
+++ Axpo.PowerTrade.App/Axpo.PowerTrade.Application/Constants/Constants.cs
@@ -3,7 +3,6 @@
 	static class FileExportConstants
 	{
 		public const string PowerPosition = "PowerPosition";
-		public const char T = 'T';
 		public const string Datetime = "Datetime";
 		public const string Volume = "Volume";
     }
